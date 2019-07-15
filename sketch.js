5a344631dd4f14eac9cf9f8f4e5ab9e29850a8f2
ehunternewton@gmail.com <ehunternewton@gmail.com>
2019-07-14 21:33:55 -0500

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -10,31 +10,38 @@ function setup() {
   size = 20;
   playArea = new PlayArea(20);
   playArea.createMap(size);
-  let start = playArea.mapTiles[floor(random(playArea.mapTiles.length))];
-  // let start = playArea.mapTiles[346];
+  // let start = playArea.mapTiles[floor(random(playArea.mapTiles.length))];
+  let start = playArea.mapTiles[346];
   start.start = true;
-  let spawnPoint = playArea.mapTiles[floor(random(playArea.mapTiles.length))];
-  // let end = playArea.mapTiles[22]
+  // let spawnPoint = playArea.mapTiles[floor(random(playArea.mapTiles.length))];
+  let spawnPoint = playArea.mapTiles[22]
   spawnPoint.spawnPoint = true;
   playArea.createWalkMap();
   //createDistanceMap(playArea);
   
   walkers = [];
-  for (let i = 0; i < 10; i++) {
+  for (let i = 0; i < 1; i++) {
     // walker = new RandomWalker(playArea.mapTiles[floor(random(playArea.mapTiles.length))], playArea.mapTiles);
     walker = new RandomWalker(spawnPoint, playArea.mapTiles);
     walkers.push(walker);
   }
   // walker = new RandomWalker(playArea.mapTiles[10], playArea.mapTiles);
 }
-
+let count = 0;
+let delay = 100;
 function draw() {
-    playArea.mouseOver();
-    playArea.show();
-    for (let i = 0; i < walkers.length; i++) {
-      walkers[i].move();
-      walkers[i].show();
-    }
+  count++;
+  if (count % delay == 0){
+    let walker = new RandomWalker(playArea.mapTiles[22], playArea.mapTiles);
+    walkers.push(walker);
+  }
+  
+  playArea.mouseOver();
+  playArea.show();
+  for (let i = 0; i < walkers.length; i++) {
+    walkers[i].move();
+    walkers[i].show();
+  }
 }
 
 // function createDistanceMap(playArea) {
