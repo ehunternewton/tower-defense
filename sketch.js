df60bf9b369fc755308cea02fcf55b36f3bab682
ehunternewton@gmail.com <ehunternewton@gmail.com>
2019-08-08 00:43:48 -0500

    messed with frame rates

--- sketch.js
+++ sketch.js
@@ -24,7 +24,7 @@ function preload() {
 }
 
 function setup() {
-  frameRate(100);
+  frameRate(60);
   canvasWidth = 1280;
   canvasHeight = 720;
   let cnv = createCanvas(canvasWidth, canvasHeight);
